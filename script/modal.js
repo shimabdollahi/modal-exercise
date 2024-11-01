a1cef9155aadf9f4668a0cd1f112173fbe7db84c
Shima Abdollahi <6imabdollahi@gmail.com>
2024-11-01 12:53:17 +0330

    modal scripts compeleted.

--- script/modal.js
+++ script/modal.js
@@ -2,13 +2,19 @@ function openModal(modal_id){
     let modal = document.getElementById(modal_id);
     modal.classList.remove("modal__none");
 }
-// function closeModal(e){
-//     let modal=e.target.closest("modal");
-//     modal.classList.add("modal__none");
-// }
 for(let btn of document.getElementsByClassName("close-modal")){
     btn.onclick=function (e) {
         let modal=e.target.closest(".modal");
         modal.classList.add("modal__none");
     }
+}
+for(let modal of document.getElementsByClassName("modal")){
+    let modalBox=modal.getElementsByClassName("modal__box")[0];
+    modalBox.addEventListener('click' , (e)=>{
+        e.stopPropagation();
+    })
+    modal.addEventListener('click' , ()=>{
+        // e.target.classList.add("modal__none")
+        modal.classList.add("modal__none")
+    });
 }
